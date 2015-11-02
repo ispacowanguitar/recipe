b7ec9e362a6b2cdba4a337fb133696b9e50aa4ed
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2015-11-02 12:48:01 -0600

    prints out day, min, max

---
+++ alex.rb
@@ -0,0 +1,30 @@
+
+data = []
+File.open("weather.dat", "r") do |f|
+  f.each_line do |line|
+    data << line
+  end
+end
+
+
+
+array = []
+min_spread = 100
+min_day = 0
+data.each do |line|
+    array = line.split(" ")
+    spread = array[1].to_f - array[2].to_f
+    if spread.to_f < min_spread && array[0] != "Dy" && array[0] != 0
+      min_spread = spread
+      min_day = array[0].to_i
+    end
+    if array[1] != nil
+      puts "Day: #{array[0]}, Max: #{array[1]}, Min: #{array[2]}, Spread: #{spread}"
+    end
+  
+end
+
+puts "the min spread day is #{min_day}"
+
+
+
