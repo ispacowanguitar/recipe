78d6fbab329ddf23c8df80e5afe54541a02cb801
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2015-11-02 14:36:34 -0600

    deleted first two lines of weather.dat because they were not numbers. now it works!

--- alex.rb
+++ alex.rb
@@ -5,8 +5,8 @@ File.open("weather.dat", "r") do |f|
     data << line
   end
 end
-
-
+data.shift
+data.shift
 
 array = []
 min_spread = 100
@@ -14,14 +14,12 @@ min_day = 0
 data.each do |line|
     array = line.split(" ")
     spread = array[1].to_f - array[2].to_f
-    if spread.to_f < min_spread && array[0] != "Dy" && array[0] != 0
+    if spread.to_f < min_spread 
       min_spread = spread
       min_day = array[0].to_i
     end
-    if array[1] != nil
-      puts "Day: #{array[0]}, Max: #{array[1]}, Min: #{array[2]}, Spread: #{spread}"
-    end
   
+    # puts "Day: #{array[0]}, Max: #{array[1]}, Min: #{array[2]}, Spread: #{spread}"
 end
 
 puts "the min spread day is #{min_day}"
